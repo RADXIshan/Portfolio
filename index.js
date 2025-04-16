00ba03e1fe021dca0040b41249cd4f35567d60b0
Ishan Roy <ishanroy@Ishans-MacBook-Air.local>
2025-04-16 14:43:57 +0530

    Adding Preloader Screen

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+window.addEventListener('load', () => {
+    const preloadScreen = document.getElementById('preload-screen');
+    const header = document.querySelector('.header');
+
+    setTimeout(() => {
+        preloadScreen.classList.add('fade-out');
+
+        setTimeout(() => {
+            header.style.opacity = '1';
+            header.style.visibility = 'visible';
+        }, 0.1); 
+    }, 3000); 
+});
+
 const greetings = document.querySelectorAll('.greeting');
 let currentIndex = 0;
 const displayDuration = 3000; // Time each greeting is shown (in milliseconds)
